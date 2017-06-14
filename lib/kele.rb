c38e8d642e47d81e4190c280eb08318bd9951c92
danielleoverman <danielleoverman1@gmail.com>
2017-06-13 23:15:16 -0400

    retrieve users completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -7,12 +8,17 @@ class Kele
 
   def initialize(email, password)
     response = self.class.post(api_url("sessions"), body: { "email": email, "password": password })
-    puts response.code
+    puts response
     raise "That is an invalid email or password" if response.code == 404
     @auth_token = response["auth_token"]
   end
 
   private
+
+  def get_me (auth_token)
+    response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
