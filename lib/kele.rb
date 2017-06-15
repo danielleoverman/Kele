a46e13b5cb8b879eeb081e8727886b38aa2dd7e6
danielleoverman <danielleoverman1@gmail.com>
2017-06-14 23:35:44 -0400

    mentor availability completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,6 +12,11 @@ class Kele
     @auth_token = response["auth_token"]
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+  end
+  
   private
 
   def api_url(endpoint)
