71940f73a57617b5f806f28db5938940bd344f4c
danielleoverman <danielleoverman1@gmail.com>
2017-06-15 21:43:04 -0400

    mentor availability working correctly?

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,11 +13,9 @@ class Kele
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
-    @mentor_availability = JSON.parse(response.body)
+     response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+     @mentor_availability = JSON.parse(response.body)
   end
-  
-  private
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
