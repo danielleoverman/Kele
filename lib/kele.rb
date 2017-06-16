4f7baf3277ad4995208729492390424081b7fd0e
danielleoverman <danielleoverman1@gmail.com>
2017-06-15 22:36:47 -0400

    working on user-story-5 roadmaps and checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,7 +12,17 @@ class Kele
     @auth_token = response["auth_token"]
   end
 
-  private
+
+  def get_roadmap()
+    response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+    @roadmap = JSON.parse(response.body)  
+  end
+
+  def get_checkpoint()
+    response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+    @checkpoint = JSON.parse(response.body)  
+  end
+
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
