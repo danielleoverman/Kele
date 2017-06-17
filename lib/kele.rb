6e4bfb704f0aacf3c97f6db5e37ba4fc9c3669d3
danielleoverman <danielleoverman1@gmail.com>
2017-06-16 21:59:26 -0400

    user story roadmaps and checkpoints completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,7 +1,11 @@
 require 'httparty'
 
+
+require './lib/roadmap.rb'
+
 class Kele
   include HTTParty
+  include Roadmap 
 
   base_uri "https://www.bloc.io/api/v1/"
 
@@ -11,18 +15,6 @@ class Kele
     raise "That is an invalid email or password" if response.code == 404
     @auth_token = response["auth_token"]
   end
-
-
-  def get_roadmap()
-    response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
-    @roadmap = JSON.parse(response.body)  
-  end
-
-  def get_checkpoint()
-    response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
-    @checkpoint = JSON.parse(response.body)  
-  end
-
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
