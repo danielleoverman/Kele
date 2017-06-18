680044f26296e09ebca272309dc177d06d89c1f9
danielleoverman <danielleoverman1@gmail.com>
2017-06-17 22:02:36 -0400

    working on create message in user story 6

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,14 +12,19 @@ class Kele
     @auth_token = response["auth_token"]
   end
 
-def get_messages(page_number=nil)
+  def get_messages(page_number=nil)
     if page_number == nil
       response = self.class.get(api_url("message_threads"), headers: { "authorization" => @auth_token })
     else
       response = self.class.get(api_url("message_threads?page=#{page_number}"), headers: { "authorization" => @auth_token })
     end    
     @messages = JSON.parse(response.body)
-end
+  end
+
+  def create_message(sender, recipient_id, token, subject, stripped)
+    options = {body: {sender: email, recipient_id: recipient_id, token: nil, subject: subject, stripped-text: stripped-text}, headers: { "authorization" => @auth_token }}
+    self.class.post(api_url("messages"), options)
+  end
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
